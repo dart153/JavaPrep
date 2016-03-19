98ded5096e9fc44cfd902f72b33bd1d5eed7613e
Pranav Iddamsetty <pranaviddamsetty@Pranavs-MacBook-Pro-3.local>
2016-03-19 15:36:41 -0700

    Initial commit- Hello

---
+++ src/main/java/com/pranav/java/Hello.java
@@ -0,0 +1,10 @@
+package com.pranav.java;
+
+/**
+ * Created by pranaviddamsetty on 3/19/16.
+ */
+public class Hello {
+    public static void main(String arg[]){
+        System.out.println("Hello");
+    }
+}
